2e05abe0635aa6586e68257377b77e3a5ad6df3f
SimonGreenhill simon <SimonGreenhill simon@simon.net.nz>
2013-04-04 16:08:02 +0000

    fixing 404 template

---
+++ website/website/apps/core/tables.py
@@ -0,0 +1,72 @@
+import django_tables2 as tables
+from django_tables2.utils import A  # alias for Accessor
+
+from website.apps.core.models import Source, Language, Family
+
+# Note, due to the current version of django_tables2 not merging in Meta classes
+# https://github.com/bradleyayers/django-tables2/issues/85
+# The work around is to inherit class Meta in the subclasses e.g.
+# class Child(DataTable):
+#     class Meta(DataTable.Meta):
+#         pass
+#
+# another annoyance is that you can't override anything in Meta - it'll cause a 
+# NameError to be raised. The work around is this:
+#
+# class Child(DataTable):
+#     class Meta(DataTable.Meta):
+#         pass
+#     Meta.var = X
+#
+# Ugly, but it works.
+
+class DataTable(tables.Table):
+    """Parent class for Datatables"""
+    class Meta:
+        orderable = True
+        default = u''
+        attrs = {
+            'class': "table table-bordered table-condensed",
+            'summary': '',
+        }
+    
+
+class SourceIndexTable(DataTable):
+    """Source Listing"""
+    author = tables.LinkColumn('source-detail', args=[A('slug')])
+    year = tables.LinkColumn('source-detail', args=[A('slug')])
+    reference = tables.LinkColumn('source-detail', args=[A('slug')])
+    
+    class Meta(DataTable.Meta):
+        model = Source
+        order_by = 'author' # default sorting
+        sequence = ('author', 'year', 'reference')
+        exclude = ('id', 'editor', 'added', 'slug', 'comment', 'bibtex')
+    Meta.attrs['summary'] = 'Table of Sources'
+    
+
+class LanguageIndexTable(DataTable):
+    """Language Listing"""
+    language = tables.LinkColumn('language-detail', args=[A('slug')])
+    isocode = tables.LinkColumn('language-detail', args=[A('slug')])
+    
+    class Meta(DataTable.Meta):
+        model = Language
+        order_by = 'language' # default sorting
+        sequence = ('isocode', 'language', 'classification')
+        exclude = ('id', 'editor', 'added', 'slug', 'information')
+    Meta.attrs['summary'] = 'Table of Languages'
+
+
+class FamilyIndexTable(DataTable):
+    """Family Listing"""
+    family = tables.LinkColumn('family-detail', args=[A('slug')])
+    count = tables.LinkColumn('family-detail', args=[A('slug')])
+    
+    class Meta(DataTable.Meta):
+        model = Family
+        order_by = 'family' # default sorting
+        sequence = ('family', 'count')
+        exclude = ('id', 'editor', 'added', 'slug')
+    Meta.attrs['summary'] = 'Table of Language Families'
+    
---
+++ website/website/apps/lexicon/models.py
@@ -0,0 +1,161 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+
+from website.apps.core.models import TrackedModel
+
+
+
+COGNATESET_QUALITY = (
+    ('0', 'Unassessed'),
+    ('1', 'Published'),
+    ('2', 'Accepted'),
+    # space for more..
+    ('9', 'Problematic'),
+)
+
+COGNATE_QUALITY = (
+    ('0', 'Unassessed'),
+    ('1', 'Published'),
+    ('2', 'Accepted'),
+    # space for more..
+    ('9', 'Problematic'),
+)
+
+WORD_QUALITY = (
+    ('0', 'Unassessed'),
+    ('1', 'Extremely stable or reliable'),
+    ('8', 'Open to serious objections as a test item'),
+    ('9', 'Highly unsuitable'),
+)
+
+class Word(TrackedModel):
+    """Word Details"""
+    word = models.CharField(max_length=64, db_index=True,
+        help_text="Word in English")
+    slug = models.SlugField(max_length=64, unique=True,
+        help_text="`Slug` for word (for use in URLS)")
+    full = models.TextField(blank=True, null=True,
+        help_text="Full word details/gloss.")
+    comment = models.TextField(blank=True, null=True,
+        help_text="PUBLIC comment on this word")
+    quality = models.CharField(default=u'0', max_length=1, choices=WORD_QUALITY,
+            help_text="The quality of this word.")
+    
+    def __unicode__(self):
+        return self.slug
+    
+    @models.permalink
+    def get_absolute_url(self):
+        return ('word-detail', [self.slug])
+    
+    class Meta:
+        db_table = 'words'
+
+
+class WordSubset(TrackedModel):
+    """Word Subset Details"""
+    subset = models.CharField(max_length=64, db_index=True,
+        help_text="Subset Label")
+    slug = models.SlugField(max_length=64, unique=True,
+        help_text="`Slug` for subset (for use in URLS)")
+    description = models.TextField(blank=True, null=True,
+        help_text="Details of subset.")
+    words = models.ManyToManyField('Word', blank=True, null=True)
+    
+    def __unicode__(self):
+        return self.slug
+        
+    @models.permalink
+    def get_absolute_url(self):
+        return ('subset-detail', [self.slug])
+        
+    class Meta:
+        db_table = 'wordsubsets'
+        verbose_name_plural = 'Word Subsets'
+
+
+class Lexicon(TrackedModel):
+    """Lexicon Details"""
+    language = models.ForeignKey('core.Language')
+    source = models.ForeignKey('core.Source')
+    word = models.ForeignKey('Word')
+    
+    entry = models.CharField(max_length=32, 
+        help_text="Entry from source")
+    phon_entry = models.CharField(max_length=32, null=True, blank=True,
+        help_text="Entry in Phonological format (in known)")
+    
+    annotation = models.TextField(blank=True, null=True,
+        help_text="Annotation for this item")
+    
+    loan = models.BooleanField(default=False, db_index=True,
+        help_text="Is a loan word?")
+    loan_source = models.ForeignKey('core.Language', blank=True, null=True, 
+        related_name = 'loan_source_set',
+        help_text="Loanword Source (if known)"
+    )
+    
+    def __unicode__(self):
+        return u"%d-%s" % (self.id, self.entry)
+
+    class Meta:
+        db_table = 'lexicon'
+        verbose_name_plural = 'Lexicon'
+
+
+class CognateSet(TrackedModel):
+    """Cognate Sets"""
+    label = models.CharField(max_length=32, blank=True, null=True)
+    source = models.ForeignKey('core.Source', 
+        null=True, blank=True)
+    comment = models.TextField(blank=True, null=True,
+        help_text="Comment about this cognate set")
+    quality = models.CharField(default=u'0', max_length=1, choices=COGNATESET_QUALITY,
+            help_text="The quality of this cognate set.")
+    lexicon = models.ManyToManyField('Lexicon', through='Cognate')
+    
+    def __unicode__(self):
+        return "%d. %s" % (self.id, self.label)
+    
+    class Meta:
+        db_table = 'cognatesets'
+        verbose_name_plural = 'Cognate Sets'
+    
+
+class Cognate(TrackedModel):
+    """Cognacy Judgements"""
+    lexicon = models.ForeignKey('Lexicon')
+    cognateset = models.ForeignKey('CognateSet')
+    source = models.ForeignKey('core.Source', 
+        null=True, blank=True)
+    comment = models.TextField(blank=True, null=True,
+        help_text="Comment about this Cognate set")
+    flag = models.CharField(default=0, max_length=1, choices=COGNATE_QUALITY,
+            help_text="The quality of this cognate.")
+    
+    def __unicode__(self):
+        return u"%d.%d" % (self.cognateset_id, self.id)
+    
+    class Meta:
+        db_table = 'cognates'
+
+
+class CorrespondenceSet(TrackedModel):
+    """Sound Correspondence Sets"""
+    language = models.ManyToManyField('core.Language', through='Correspondence')
+    source = models.ForeignKey('core.Source', blank=True, null=True)
+    comment = models.TextField(blank=True, null=True, help_text="Notes")
+    
+    class Meta:
+        db_table = 'corrsets'
+        verbose_name_plural = 'Correspondence Sets'
+        
+
+class Correspondence(TrackedModel):
+    """Sound Correspondence Rules"""
+    language = models.ForeignKey('core.Language')
+    corrset = models.ForeignKey('CorrespondenceSet')
+    rule = models.CharField(max_length=5)
+    
+    class Meta:
+        db_table = 'correspondences'
