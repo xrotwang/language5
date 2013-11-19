da8b4e6147ccd8dbdd0142ef4709bae95c4a303f
Simon Greenhill <simon@simon.net.nz>
2013-11-19 21:13:04 +1100

    adding migrations

---
+++ website/website/apps/lexicon/models.py
@@ -0,0 +1,204 @@
+from django.db import models
+from django.db.models.signals import pre_save
+from django.core.urlresolvers import reverse
+
+import watson
+
+from website.apps.core.models import TrackedModel
+from website.apps.statistics import statistic
+from website.signals import create_redirect
+
+
+
+COGNATESET_QUALITY = (
+    ('0', 'Unassessed'),
+    ('1', 'Published'),
+    ('2', 'Accepted'),
+    # space for more..
+    ('9', 'Problematic'),
+)
+
+COGNATE_QUALITY = (
+    ('0', 'Unassessed'),
+    ('1', 'Published'),
+    ('2', 'Accepted'),
+    # space for more..
+    ('9', 'Problematic'),
+)
+
+WORD_QUALITY = (
+    ('0', 'Unassessed'),
+    ('1', 'Extremely stable or reliable'),
+    ('8', 'Open to serious objections as a test item'),
+    ('9', 'Highly unsuitable'),
+)
+
+class Word(TrackedModel):
+    """Word Details"""
+    word = models.CharField(max_length=64, db_index=True, unique=True,
+        help_text="Word in English")
+    slug = models.SlugField(max_length=64, unique=True,
+        help_text="`Slug` for word (for use in URLS)")
+    full = models.TextField(blank=True, null=True,
+        help_text="Full word details/gloss.")
+    comment = models.TextField(blank=True, null=True,
+        help_text="PUBLIC comment on this word")
+    quality = models.CharField(default=u'0', max_length=1, choices=WORD_QUALITY,
+            help_text="The quality of this word.")
+    
+    def __unicode__(self):
+        if self.full:
+            return u"%s (%s)" % (self.word, self.full)
+        else:
+            return self.word
+    
+    @property
+    def fullword(self): 
+        return self.__unicode__()
+    
+    @models.permalink
+    def get_absolute_url(self):
+        return ('word-detail', [self.slug])
+    
+    class Meta:
+        db_table = 'words'
+        ordering = ['word', ]
+
+
+
+
+class WordSubset(TrackedModel):
+    """Word Subset Details"""
+    subset = models.CharField(max_length=64, db_index=True, unique=True, 
+        help_text="Subset Label")
+    slug = models.SlugField(max_length=64, unique=True,
+        help_text="`Slug` for subset (for use in URLS)")
+    description = models.TextField(blank=True, null=True,
+        help_text="Details of subset.")
+    words = models.ManyToManyField('Word', blank=True, null=True)
+    
+    def __unicode__(self):
+        return self.slug
+        
+    @models.permalink
+    def get_absolute_url(self):
+        return ('subset-detail', [self.slug])
+        
+    class Meta:
+        db_table = 'wordsubsets'
+        verbose_name_plural = 'Word Subsets'
+        ordering = ['slug', ]
+
+
+class Lexicon(TrackedModel):
+    """Lexicon Details"""
+    language = models.ForeignKey('core.Language')
+    source = models.ForeignKey('core.Source')
+    word = models.ForeignKey('Word')
+    
+    entry = models.CharField(max_length=128, db_index=True, 
+        help_text="Entry from source")
+    phon_entry = models.CharField(max_length=128, null=True, blank=True,
+        help_text="Entry in Phonological format (in known)")
+    
+    annotation = models.TextField(blank=True, null=True,
+        help_text="Annotation for this item")
+    
+    loan = models.BooleanField(default=False, db_index=True,
+        help_text="Is a loan word?")
+    loan_source = models.ForeignKey('core.Language', blank=True, null=True, 
+        related_name = 'loan_source_set',
+        help_text="Loanword Source (if known)"
+    )
+    
+    def __unicode__(self):
+        return u"%d-%s" % (self.id, self.entry)
+
+    class Meta:
+        db_table = 'lexicon'
+        verbose_name_plural = 'Lexical Items'
+        ordering = ['entry', ]
+
+
+class CognateSet(TrackedModel):
+    """Cognate Sets"""
+    protoform = models.CharField(max_length=128, blank=True, null=True, db_index=True)
+    gloss = models.CharField(max_length=128, blank=True, null=True)
+    comment = models.TextField(blank=True, null=True,
+        help_text="Comment about this cognate set")
+    source = models.ForeignKey('core.Source', 
+        null=True, blank=True)
+    lexicon = models.ManyToManyField('Lexicon', through='Cognate')
+    quality = models.CharField(default=u'0', max_length=1, choices=COGNATESET_QUALITY,
+            help_text="The quality of this cognate set.")
+    
+    def __unicode__(self):
+        return "%d. %s '%s'" % (self.id, self.protoform, self.gloss)
+    
+    class Meta:
+        db_table = 'cognatesets'
+        verbose_name_plural = 'Cognate Sets'
+    
+
+class Cognate(TrackedModel):
+    """Cognacy Judgements"""
+    lexicon = models.ForeignKey('Lexicon')
+    cognateset = models.ForeignKey('CognateSet')
+    source = models.ForeignKey('core.Source', 
+        null=True, blank=True)
+    comment = models.TextField(blank=True, null=True,
+        help_text="Comment about this Cognate set")
+    flag = models.CharField(default=0, max_length=1, choices=COGNATE_QUALITY,
+            help_text="The quality of this cognate.")
+    
+    def __unicode__(self):
+        return u"%d.%d" % (self.cognateset_id, self.id)
+    
+    class Meta:
+        db_table = 'cognates'
+
+
+class CorrespondenceSet(TrackedModel):
+    """Sound Correspondence Sets"""
+    language = models.ManyToManyField('core.Language', through='Correspondence')
+    source = models.ForeignKey('core.Source', blank=True, null=True)
+    comment = models.TextField(blank=True, null=True, help_text="Notes")
+    
+    def __unicode__(self):
+        return u"Correspondence Set: %s" % self.comment
+    
+    class Meta:
+        db_table = 'corrsets'
+        verbose_name_plural = 'Correspondence Sets'
+        
+
+class Correspondence(TrackedModel):
+    """Sound Correspondence Rules"""
+    language = models.ForeignKey('core.Language')
+    corrset = models.ForeignKey('CorrespondenceSet')
+    rule = models.CharField(max_length=5)
+    
+    def __unicode__(self):
+        return u"Correspondence: %s /%s/" % (self.language.slug, self.rule)
+    
+    class Meta:
+        db_table = 'correspondences'
+
+
+# pre-save adding of redirects when slug field altered.
+pre_save.connect(create_redirect, sender=Word, dispatch_uid="word:001")
+pre_save.connect(create_redirect, sender=WordSubset, dispatch_uid="wordsubset:001")
+
+
+watson.register(Word, fields=('word', 'full'))
+watson.register(WordSubset, fields=('subset', 'description'))
+watson.register(Lexicon, fields=('entry', 'annotation'))
+
+
+statistic.register("Number of Words", Word, graph=3)
+statistic.register("Number of Word Sets", WordSubset)
+statistic.register("Number of Lexical Items", Lexicon, graph=4)
+statistic.register("Number of Cognates", Cognate)
+statistic.register("Number of Cognate Sets", CognateSet)
+statistic.register("Number of Correspondences", Correspondence)
+statistic.register("Number of Correspondence Sets", CorrespondenceSet)
---
+++ website/website/apps/pronouns/tools/tools.py
@@ -0,0 +1,162 @@
+from django.db.models import Count
+from website.apps.pronouns.models import Paradigm, PronounType, Pronoun
+from website.apps.pronouns.models import ALIGNMENT_CHOICES, PERSON_CHOICES, NUMBER_CHOICES, GENDER_CHOICES
+
+def full_repr_row(p):
+    """Build a string representation of the given pronoun `p`"""
+    
+    def _get(key, choices):
+        if isinstance(key, basestring):
+            for x, y in choices:
+                if key == x: 
+                    return y
+        else:
+            return key[1]
+    
+    # handle objects
+    if isinstance(p, Pronoun):
+        if p.pronountype.gender is None:
+            return " ".join([p.pronountype.get_person_display(), p.pronountype.get_number_display()])
+        else:
+            return " ".join([p.pronountype.get_person_display(), p.pronountype.get_number_display(), p.pronountype.get_gender_display()])
+    elif isinstance(p, PronounType):
+        if p.gender is None:
+            return " ".join([p.get_person_display(), p.get_number_display()])
+        else:
+            return " ".join([p.get_person_display(), p.get_number_display(), p.get_gender_display()])
+    # handle dictionary
+    else:
+        person = _get(p['person'], PERSON_CHOICES)
+        number = _get(p['number'], NUMBER_CHOICES)
+        
+        if p['gender'] is None:
+            return " ".join([person, number])
+        else:
+            gender = _get(p['gender'], GENDER_CHOICES)
+            return " ".join([person, number, gender])
+
+
+def short_repr_row(p):
+    """Builds a short string representation of the given pronoun `p`"""
+    if isinstance(p, Pronoun):
+        newp = {
+            'person': p.pronountype.person,
+            'number': p.pronountype.number,
+            'alignment': p.pronountype.alignment,
+            'gender': p.pronountype.gender,
+        }
+    elif isinstance(p, PronounType):
+        newp = {
+            'person': p.person,
+            'number': p.number,
+            'alignment': p.alignment,
+            'gender': p.gender,
+        }
+    else:
+        newp = {
+            'person': p['person'][0],
+            'number': p['number'][0],
+            'alignment': p['alignment'][0],
+        }
+        if p['gender'] is not None:
+            newp['gender'] = p['gender'][0]
+        else:
+            newp['gender'] = None
+
+    if newp['gender'] is None:
+        return "%(person)s%(number)s %(alignment)s" % newp
+    else:
+        return "%(person)s%(number)s %(gender)s %(alignment)s" % newp
+
+def add_pronoun_table(pronoun_set, filter_empty_rows=True):
+    """Construct a table for the given pronoun set
+    
+    `filter_empty_rows` - leave out the rows that are empty.
+    """
+    # loop over the pronouns we've been given and fill a table of the cells.
+    cells = {}
+    for p in pronoun_set:
+        label = full_repr_row(p)
+        # get row or set it to (A, None), (S, None), (O, None), (P, None)
+        # i.e. empty placeholders for each different ALIGNMENT
+        cells[label] = cells.get(label, 
+            dict(zip([_[0] for _ in ALIGNMENT_CHOICES], 
+                     [None for _ in ALIGNMENT_CHOICES]))
+        )
+        # Save the pronoun into the right row/alignment cell.
+        cells[label][p.pronountype.alignment] = p
+    # Now do the sorting of the table *rows*
+    pronoun_rows = []
+    # Sort
+    ptype_rows = PronounType._generate_all_rows()
+    for row in ptype_rows:
+        wanted_label = full_repr_row(row)
+        found_row = False
+        # go through each label in the cells e.g. (1st person singular...etc)
+        for label in cells:
+            if wanted_label == label:
+                found_row = True
+                # Ignore empty rows?
+                # only add this row if at LEAST one cell has something in it.
+                if filter_empty_rows: 
+                    non_zero = 0
+                    for cell, value in cells[label].items():
+                        if value is not None and len(value.entries.all()) > 0:
+                            non_zero += 1
+                    if non_zero: # at least one cell is not empty
+                        pronoun_rows.append((label, cells[label]))
+                else:
+                    pronoun_rows.append((label, cells[label]))
+        assert found_row, \
+        "Unable to find expected row for Paradigm: %s - probably need to run _prefill_pronouns()" % wanted_label
+    if not filter_empty_rows: 
+        assert len(pronoun_rows) == len(ptype_rows)
+    return pronoun_rows
+
+
+def find_identicals(paradigm):
+    """Find identical forms in the given list of `pronouns`"""
+    identical = set()
+    pronouns = paradigm.pronoun_set.annotate(entry_count=Count('entries')).exclude(entry_count=0)
+    pronouns = pronouns.values_list('id', 'entries__id', 'entries__entry')
+    
+    for pronoun1 in pronouns:
+        for pronoun2 in pronouns:
+            if pronoun1[1] == pronoun2[1]:
+                continue # ignore self.
+                
+            if pronoun1[2] == pronoun2[2]:
+                # same, try to order them consistently
+                if pronoun1[1] < pronoun2[1]:
+                    o = (pronoun1, pronoun2)
+                else:
+                    o = (pronoun2, pronoun1)
+                identical.add(o)
+    return identical
+    
+
+def extract_rule(values):
+    rule = {'one': {}, 'two': {}, 'relationship': None}
+    for key, value in values.items():
+        if value == '---':
+            # ignore empty
+            continue
+        elif key == 'relationship':
+            rule['relationship'] = value
+        else:
+            param, subset = key.split("_")
+            if param in ('gender', 'person', 'alignment', 'number'):
+                rule[subset][param] = value
+    
+    # check some things
+    if rule['relationship'] is None:
+        raise ValueError("Must have a relationship value")
+    if len(rule['one']) == 0:
+        raise ValueError("No operands set for side 1")
+    if len(rule['two']) == 0:
+        raise ValueError("No operands set for side two")
+        
+    # all good, return rule.
+    return rule
+
+
